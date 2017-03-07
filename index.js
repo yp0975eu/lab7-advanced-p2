567510455537b070bab23070f31044308ba2125d
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-03-07 12:21:37 -0600

    end of chapter 3

--- index.js
+++ index.js
@@ -6,31 +6,33 @@ var handlebars = require('express3-handlebars').create({defaultLayout: "main"});
 app.engine('handlebars',  handlebars.engine );
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
 
-
-app.get('/', function(req, res){ res.type('text/plain');
-    res.send('Your Fortune');
+app.get('/', function(req, res) { res.render('home');
 });
-
-app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('About Your Fortune');
+app.get('/about', function(req, res) {
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
 });
-
-// custom 404 page
-app.use(function(req, res){ res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){ res.status(404);
+    res.render('404');
 });
-
-// custom 500 page
+// 500 error handler (middleware)
 app.use(function(err, req, res, next){ console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate.' );
 });
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple."
+]
